1679304510d51adc3fc4ee3903c4e7d05412f4c3
Karel J. van der Lelij <kadmosb1@users.noreply.github.com>
2021-04-09 08:18:12 +0200

    User Story ready

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println ("Taak 1 heb ik uitgevoerd");
-
+        System.out.println ("Taak 2 heb ik ook uitgevoerd");
     }
 }
